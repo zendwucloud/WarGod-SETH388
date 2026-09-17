0424c394d5051798241d461e1a257099a17120b9
zendwucloud <zendwucloud@gmail.com>
2026-09-17 23:27:24 +0800

    風暴水晶動畫

--- application.40221.js
+++ application.40221.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.f6dce.json';
+          this.settingsPath = 'src/settings.c708d.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.0e759.js
+++ index.0e759.js
@@ -1,4 +1,4 @@
-System.register(["./application.42620.js"], function (_export, _context) {
+System.register(["./application.40221.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
